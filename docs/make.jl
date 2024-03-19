3e0fd2f6441da9367d1d7297dacefa09eed784f0
Hongyang Zhou <hyzhou@umich.edu>
2024-03-19 12:10:52 -0400

    No need for repo arg in Documenter 1.0

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,6 @@ using Documenter
 makedocs(;
     modules=[FieldTracer],
     authors="Hongyang Zhou <hyzhou@umich.edu>",
-    repo="https://github.com/henry2004y/FieldTracer.jl/blob/{commit}{path}#L{line}",
     sitename="FieldTracer.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
