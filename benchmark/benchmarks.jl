4053437e97c6fa213c0a76a61b438f7dcc742759
Hongyang Zhou <hyzhou@umich.edu>
2024-06-09 15:30:37 -0400

    Add precompilation (#48)
    * Add precompilation; drop Requires
    
    * Update benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -10,7 +10,7 @@ SUITE["trace"]["2D structured"] = BenchmarkGroup()
 SUITE["trace"]["3D structured"] = BenchmarkGroup()
 
 # Euler2 and RK4 functions
-ds, maxstep = 0.1, 150
+ds, maxstep = 0.1, 100
 x = range(0, 1, step=0.1)
 y = range(0, 1, step=0.1)
 # ndgrid
@@ -22,10 +22,12 @@ xstart = 0.1
 ystart = 0.9
 
 SUITE["trace"]["2D structured"]["euler"] =
-   @benchmarkable FieldTracer.euler($maxstep, $ds, $xstart, $ystart, $x, $y, $u, $v)
+   @benchmarkable trace($u, $v, $xstart, $ystart, $x, $y;
+      alg=Euler(), ds=$ds, maxstep=$maxstep)
 
 SUITE["trace"]["2D structured"]["rk4"] =
-   @benchmarkable FieldTracer.rk4($maxstep, $ds, $xstart, $ystart, $x, $y, $u, $v)
+   @benchmarkable trace($u, $v, $xstart, $ystart, $x, $y;
+      alg=RK4(), ds=$ds, maxstep=$maxstep)
 
 x = range(0, 10, length=15)
 y = range(0, 10, length=20)
@@ -36,8 +38,7 @@ bz = fill(1.0, length(x), length(y), length(z))
 xs, ys, zs = 1.0, 1.0, 1.0
 
 SUITE["trace"]["3D structured"]["euler"] =
-   @benchmarkable trace($bx, $bz, $bz, $xs, $ys, $zs, $x, $y, $z;
-      alg=Euler(), ds=0.2, maxstep=200)
+   @benchmarkable trace($bx, $by, $bz, $xs, $ys, $zs, $x, $y, $z;
+      alg=Euler(), ds=$ds, maxstep=$maxstep)
 SUITE["trace"]["3D structured"]["rk4"] =
-   @benchmarkable  trace($bx, $by, $bz, $xs, $ys, $zs, $x, $y, $z;
-      ds=0.2, maxstep=200, direction="forward")
+   @benchmarkable trace($bx, $by, $bz, $xs, $ys, $zs, $x, $y, $z; ds=$ds, maxstep=$maxstep)
