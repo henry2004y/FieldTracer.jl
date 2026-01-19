e85de5762451d1fca804306af21da518f285fd3c
Hongyang Zhou <hyzhou@umich.edu>
2026-01-19 18:22:36 -0500

    Redesign precompilation

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,36 +1,42 @@
 # Precompiling workloads
 
 @setup_workload begin
-	@compile_workload begin
-		x = range(0, 1, step = 0.1)
-		y = range(0, 1, step = 0.1)
-		# ndgrid
-		u, v = let
-			xgrid = [i for i in x, _ in y]
-			ygrid = [j for _ in x, j in y]
-			copy(xgrid), -copy(ygrid)
-		end
-		xstart = 0.1
-		ystart = 0.9
+    @compile_workload begin
+        # 2D Data
+        x = range(0, 1, step = 0.1)
+        y = range(0, 1, step = 0.1)
+        u = fill(1.0, length(x), length(y))
+        v = fill(1.0, length(x), length(y))
 
-		xt, yt = trace(u, v, xstart, ystart, x, y;
-			maxstep = 100, ds = 0.1, direction = "both", alg = Euler())
-		xt, yt = trace(u, v, xstart, ystart, x, y;
-			maxstep = 100, ds = 0.1, direction = "both", alg = RK4())
+        # Single particle
+        xstart, ystart = 0.5, 0.5
+        trace(u, v, xstart, ystart, x, y; maxstep = 10, ds = 0.1, alg = Euler())
+        trace(u, v, xstart, ystart, x, y; maxstep = 10, ds = 0.1, alg = RK4())
 
-		x = range(0, 10, length = 10)
-		y = range(0, 10, length = 10)
-		z = range(0, 10, length = 10)
-		bx = fill(1.0, length(x), length(y), length(z))
-		by = fill(1.0, length(x), length(y), length(z))
-		bz = fill(1.0, length(x), length(y), length(z))
-		xs, ys, zs = 1.0, 1.0, 1.0
+        # Batch particles (Vector input)
+        xstarts = [0.1, 0.5, 0.9]
+        ystarts = [0.1, 0.5, 0.9]
+        trace(u, v, xstarts, ystarts, x, y; maxstep = 10, ds = 0.1, alg = Euler())
+        trace(u, v, xstarts, ystarts, x, y; maxstep = 10, ds = 0.1, alg = RK4())
 
-		# Euler 2nd order
-		x1, y1, z1 = trace(bx, by, bz, xs, ys, zs, x, y, z;
-			maxstep = 10, ds = 0.2, direction = "both", alg = Euler())
-		# RK4
-		x1, y1, z1 = trace(bx, by, bz, xs, ys, zs, x, y, z;
-			maxstep = 10, ds = 0.2, direction = "both", alg = RK4())
-	end
+        # 3D Data
+        x3 = range(0, 1, length = 5)
+        y3 = range(0, 1, length = 5)
+        z3 = range(0, 1, length = 5)
+        bx = fill(1.0, 5, 5, 5)
+        by = fill(1.0, 5, 5, 5)
+        bz = fill(1.0, 5, 5, 5)
+
+        # Single particle
+        xs, ys, zs = 0.5, 0.5, 0.5
+        trace(bx, by, bz, xs, ys, zs, x3, y3, z3; maxstep = 5, ds = 0.1, alg = Euler())
+        trace(bx, by, bz, xs, ys, zs, x3, y3, z3; maxstep = 5, ds = 0.1, alg = RK4())
+
+        # Batch particles
+        xstarts3 = [0.1, 0.5]
+        ystarts3 = [0.1, 0.5]
+        zstarts3 = [0.1, 0.5]
+        trace(bx, by, bz, xstarts3, ystarts3, zstarts3, x3, y3, z3; maxstep = 5, ds = 0.1, alg = Euler())
+        trace(bx, by, bz, xstarts3, ystarts3, zstarts3, x3, y3, z3; maxstep = 5, ds = 0.1, alg = RK4())
+    end
 end
